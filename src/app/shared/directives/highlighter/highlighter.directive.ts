3f58a03033fe26ad98b23fbc0b38113b8f7e2d6b
hvpulok <hvpulok@gmail.com>
2017-06-29 22:39:10 -0400

    Add mouse hover highligher

--- src/app/shared/directives/highlighter/highlighter.directive.ts
+++ src/app/shared/directives/highlighter/highlighter.directive.ts
@@ -1,12 +1,28 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
 })
 export class HighlighterDirective {
 
-  constructor(el: ElementRef) {
-    el.nativeElement.style.color = 'red';
+  @Input() defaultColor = 'lightBlue';
+
+  @Input('appHighlighter') highlightColor;
+
+  constructor(private el: ElementRef) {
+    this.highlight('white')  // initialize with default highlight color
   }
 
+  @HostListener('mouseenter') onmouseenter() {
+    this.highlight(this.highlightColor);
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    this.highlight('white');
+  }
+
+  highlight(color?: string) {
+    this.el.nativeElement.style.backgroundColor = color || this.defaultColor;
+    this.el.nativeElement.style.padding = '10px';
+  }
 }
